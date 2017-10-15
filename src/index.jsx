f88a3fc5b39a1f5609f77159fb0b1b234a25bc8c
evanpeng <oneword416@gmail.com>
2017-10-15 14:28:13 +0800

    install dependency moudules and webpack config initialization

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+window.onload = function() {
+    ReactDOM.render(
+        <div>Hello React!!!</div>,
+        document.getElementById('root')
+    );
+};
